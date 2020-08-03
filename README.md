a06b0b0b6da08c3003d4915f978ccc070c5733a5
Franco Benner <francobenner@gmail.com>
2020-08-03 15:34:10 -0400

    add bash deploy script and public ip requirement

--- README.md
+++ README.md
@@ -4,9 +4,28 @@
 - `Docker`
 - `Docker-Compose`
 - `Servicenode Private Key`
+- `Port 80 must be opened on the host`
 
-# Usage
+# Recommended Usage
 ```
+# Run deploy.sh
+./deploy.sh
+
+# Install Docker? [y/n] n
+# Not installing Docker...
+# Your Public IP Address: x.x.x.x
+# Servicenode Name: example
+# Servicenode Private Key: example
+# Servicenode Address: example
+
+# Please specify an user and password for the new servicenode
+# RPC Username: example
+# RPC Password: example
+```
+
+# Manual Usage
+```
+export PUBLIC_IP=""  # Update with your public ip address
 export SN_NAME="servicenode01"  # Update with your snode name
 export SN_KEY="servicenodeprivatekey"  # Update with your snode private key
 export SN_ADDRESS="servicenodekeyaddress"  # Update with your snode address
--- scripts/start-snode.sh
+++ scripts/start-snode.sh
@@ -47,7 +47,7 @@ cat > /opt/blockchain/config/xrouter.conf << EOL
 #! DNS and ip addresses are acceptable values.
 #! host=mynode.example.com
 #! host=208.67.222.222
-host=xr_proxy
+host=${PUBLIC_IP}
 wallets=BLOCK
 plugins=eth_accounts,eth_blockNumber,eth_call,eth_chainId,eth_estimateGas,eth_gasPrice,eth_getBalance,eth_getBlockByHash,eth_getBlockByNumber,eth_getBlockTransactionCountByHash,eth_getBlockTransactionCountByNumber,eth_getCode,eth_getLogs,eth_getStorageAt,eth_getTransactionByBlockHashAndIndex,eth_getTransactionByBlockNumberAndIndex,eth_getTransactionByHash,eth_getTransactionCount,eth_getTransactionReceipt,eth_getUncleByBlockHashAndIndex,eth_getUncleByBlockNumberAndIndex,eth_getUncleCountByBlockHash,eth_getUncleCountByBlockNumber,eth_getWork,eth_hashrate,eth_mining,eth_protocolVersion,eth_sendRawTransaction,eth_submitWork,eth_syncing,eth_uninstallFilter,net_listening,net_peerCount,net_version,web3_clientVersion,web3_sha3,parity_allTransactionHashes,parity_allTransactions,eth_newBlockFilter,eth_newPendingTransactionFilter,eth_getFilterChanges,eth_getFilterLogs,eth_newFilter,eth_unsubscribe,parity_unsubscribe
 
