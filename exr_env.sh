6258064c737b9c4d46d0eef8339cd21e96baf285
pom11 <parsecoriginalmastercraft@gmail.com>
2022-03-29 20:19:51 +0300

    misc

--- exr_env.sh
+++ exr_env.sh
@@ -155,7 +155,7 @@ function checkdocker() {
 		printf "%s\n\033[93;1mDocker found\033[0m"
 		DOCKER_VERSION=$(docker --version | awk '{print $3}')
 		DOCKER_VERSION={{$DOCKER_VERSION::-1}}
-		if [ "$(printf '%s\n' "$dockerversion" "$DOCKER_VERSION" | sort --version-sort | head -n1)" = "$requiredver" ]; then 
+		if [ "$(printf '%s\n' "$dockerversion" "$DOCKER_VERSION" | sort --version-sort | head -n1)" = "$dockerversion" ]; then 
 			printf "%s\n\033[93;1mGreater than or equal to ${dockerversion}\033[0m"
 		else
 			printf "%s\n\033[91;1mLess than ${dockerversion}. You have to upgrade\033[0m"
@@ -174,7 +174,7 @@ function checkdockercompose() {
 	if grep -q "/" <<< "$COMPOSE"; then
 		printf "%s\n\033[93;1mDocker Compose found\033[0m"
 		COMPOSE_VERSION=$(docker-compose --version | awk -F'v' '{print $3}')
-		if [ "$(printf '%s\n' "$dockerversion" "$DOCKER_VERSION" | sort --version-sort | head -n1)" = "$requiredver" ]; then 
+		if [ "$(printf '%s\n' "$dockerversion" "$DOCKER_VERSION" | sort --version-sort | head -n1)" = "$composeversion" ]; then 
 			printf "%s\n\033[93;1mGreater than or equal to ${composeversion}\033[0m"
 		else
 			printf "%s\n\033[91;1mLess than ${composeversion}. You have to upgrade\033[0m"
